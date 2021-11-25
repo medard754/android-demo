eff94571e86c4f5a1f15cbe1585f839a7cbbbd3e
medard754 <medardboton@gmail.com>
2021-11-25 11:33:23 +0100

    créaction de l'activité

---
+++ app/src/main/java/medard754/demo/calculatriceActivity.java
@@ -0,0 +1,14 @@
+package medard754.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class calculatriceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculatrice);
+    }
+}
